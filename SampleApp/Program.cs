2ab39850d75e14945f9836e78eb6ff1e75152757
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-07-11 10:55:36 +0530

    Add project files.

---
+++ SampleApp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace SampleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // accept two int numbers and find the sum - console app
+
+            // Step 1: variable declarations
+            int fno;
+            int sno;
+            int sum = 0;
+
+            // Step 2: accept the input
+            Console.Write("Enter First Number:");
+            fno = int.Parse(Console.ReadLine());
+            Console.Write("Enter Second Number:");
+            sno = int.Parse(Console.ReadLine());
+
+            // Step 3: find the sum
+            sum = fno + sno;
+
+            // Step 4: Display the result
+            Console.WriteLine($"The sum of {fno} and {sno} is {sum}");
+
+
+        }
+    }
+}
