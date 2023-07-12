4d0fae3262228cacb1b5e13a14653e0e82d82edb
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-07-12 13:30:13 +0530

    oo

--- OOProgrammingDemo/Program.cs
+++ OOProgrammingDemo/Program.cs
@@ -4,20 +4,65 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            // object instatiation
             Employee e = new Employee();
-            int a;
-            a = 10;
-            e.name = "venkat";
-            e.salary = 500000;
+            e.Name = "sdsdfsd";
+            e.Salary = -1000;
+            Console.WriteLine(e.Salary);
+
+
+            Product p1 = new Product();
+            p1.ProductID = 1;
+            p1.Name = "IPhone 14 Pro";
+            p1.Rate = 1000;
+            p1.Year = 2023;
+            p1.Model = "Smart Phone";
+
+
         }
     }
 
     class Employee
     {
         //Employee e;
-        public string name;
-        public int salary;
+        //private string name;
+        //private string lname;
+        private int salary; // field
+
+        public DateTime DOB { set; get; }
+
+        public int DeptNo { get; set; }
+
+        //private string backingfiled234234234234;
+
+
+        public string Name // automatic properties
+        {
+            get;// { return backingfiled234234234234; }
+            set;// { backingfiled234234234234 = value; }
+        }
+        public int Salary // Property
+        {
+            get { return salary; }
+            set
+            {
+                if (value < 1000)
+                    salary = 1000;
+                else
+                    salary = value;
+            }
+        }
+
+    }
+
+
+
+    class Product
+    {
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public int Rate { get; set; }
+        public string Model { get; set; }
+        public string Country { get; set; }
+        public int Year { get; set; }
     }
 }
