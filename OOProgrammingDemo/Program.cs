082a58d349802088ba7d0aa31bf8e4c40051a762
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-07-12 11:53:46 +0530

    ...

--- OOProgrammingDemo/Program.cs
+++ OOProgrammingDemo/Program.cs
@@ -5,6 +5,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            // object instatiation
+            Employee e = new Employee();
+            int a;
+            a = 10;
+            e.name = "venkat";
+            e.salary = 500000;
         }
+    }
+
+    class Employee
+    {
+        //Employee e;
+        public string name;
+        public int salary;
     }
 }
