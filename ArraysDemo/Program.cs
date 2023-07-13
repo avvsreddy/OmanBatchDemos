99f341079c2bce7b07a9b9f5617c0a9a8e0bf88f
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-07-13 11:10:15 +0530

    oo

---
+++ ArraysDemo/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace ArraysDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Two-Dimensional Array
+            int[,] twod = new int[4, 6];
+        }
+
+        private static void Array4()
+        {
+            int[] numbers = { 23, 56, 23, 65, 87, 96, 34, 75 };
+            // find sum
+            int sum = numbers.Sum();
+            //foreach (int n in numbers)
+            //{
+            //    sum+= n;
+            //}
+            //Console.WriteLine(sum);
+
+            // find max
+            int max = numbers.Max();
+            // find min
+            int min = numbers.Min();
+            // find avg
+            double avg = numbers.Average();
+            // find count
+            int count = numbers.Count();
+
+            Array.Sort(numbers);
+            Array.BinarySearch(numbers, 12);
+        }
+
+        private static void ArrayDemo2()
+        {
+            int[] numbers = new int[5] { 1, 2, 3, 4, 5 };
+            int[] numbers2 = new int[] { 1, 2, 3, 4, 5 };
+            int[] numbers3 = { 1, 2, 3, 4, 5 };
+        }
+
+        private static void ArrayDemo1()
+        {
+            // i want to store 10 int numbers
+            //int n1, n2, n3, n4;
+            int[] numbers = new int[5];
+            numbers[0] = 1;
+            numbers[1] = 2;
+            numbers[2] = 3;
+            numbers[3] = 4;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+
+            foreach (int n in numbers)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
---
+++ DynamicCollectionsDemo1/Program.cs
@@ -0,0 +1,79 @@
+﻿namespace DynamicCollectionsDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Dynamic Collections
+            // store n number of ints and display
+            //DynamicArray<int> numbers = new DynamicArray<int>();
+            DynamicArray<string> numbers = new DynamicArray<string>();
+            //DynamicDoubleArray dnumbers = new DynamicDoubleArray();
+            numbers.Add("1.34");
+            numbers.Add("2.23");
+            numbers.Add("3.54");
+
+            //numbers.Add(1);
+            //numbers.Add(2);
+            //numbers.Add(3);
+            //numbers.Add(1);
+            //numbers.Add(2);
+            //numbers.Add(3);
+            //numbers.Add(1);
+            //numbers.Add(2);
+            //numbers.Add(3);
+            //numbers.Add(1);
+            //numbers.Add(2);
+            //numbers.Add(3);
+            //numbers.Add(1);
+            //numbers.Add(2);
+            numbers.Add("100");
+
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                string n = numbers.GetValue(i);
+                Console.WriteLine(n);
+            }
+        }
+
+
+        // Generic class
+        class DynamicArray<T>
+        {
+            private T[] data = new T[10];
+            private int index = 0;
+            public void Add(T v)
+            {
+                // if empty
+                if (index < data.Length)
+                {
+                    data[index++] = v;
+                }
+                else // if not empty
+                {
+                    T[] temp = new T[data.Length * 2];
+                    for (int i = 0; i < data.Length; i++)
+                    {
+                        temp[i] = data[i];
+
+                    }
+                    data = temp;
+                    data[index++] = v;
+                }
+
+            }
+
+            public int Count
+            {
+                get { return index; }
+            }
+
+            public T GetValue(int i)
+            {
+                return data[i];
+            }
+        }
+
+    }
+}
