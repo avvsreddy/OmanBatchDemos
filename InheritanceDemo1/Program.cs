9eb1c4b9ebd80bf10740b1b7fcfb8cba05245434
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-07-14 11:54:43 +0530

    oo labs

---
+++ InheritanceDemo1/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace InheritanceDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inheritance Demo");
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Country { get; set; }
+    }
+
+    class Customer : Person
+    {
+
+
+        public int CustType { get; set; }
+    }
+
+    class Employee : Person
+    {
+        public int Salary { get; set; }
+    }
+}
--- TrainerTraineeCasestudy/Program.cs
+++ TrainerTraineeCasestudy/Program.cs
@@ -2,27 +2,73 @@
 {
     internal class Program
     {
+
+
         static void Main(string[] args)
         {
+
             Console.WriteLine("Hello, World!");
+            Training dotNetTraining = new Training();
+            Trainer venkat = new Trainer();
+            dotNetTraining.Trainer = venkat;
+            Organization org = new Organization();
+            org.Name = "IIHT";
+            venkat.Organization = org;
+            Console.WriteLine("Org Name: " + dotNetTraining.GetTrainingOrgnizationName());
+
+            // get trainees count
+            Trainee t1 = new Trainee();
+            Trainee t2 = new Trainee();
+            dotNetTraining.Trainees.Add(t1);
+            dotNetTraining.Trainees.Add(t2);
+            Console.WriteLine("Trainees Count " + dotNetTraining.GetNumberOfTrainees());
+
+            // get the training duration
+
+
+            Course course = new Course();
+            dotNetTraining.Course = course;
+            Module m1 = new Module();
+            Module m2 = new Module();
+            course.Modules.Add(m1);
+            course.Modules.Add(m2);
+
+            Unit u1 = new Unit { Duration = 10 };
+            Unit u2 = new Unit { Duration = 20 };
+            m1.Units.Add(u1);
+            m1.Units.Add(u2);
+            Unit u3 = new Unit { Duration = 10 };
+            Unit u4 = new Unit { Duration = 20 };
+            m2.Units.Add(u3);
+            // m2.Units.Add(u4);
+            Console.WriteLine("Duration :" + dotNetTraining.GetTrainingDuration());
         }
+
     }
 
     public class Organization
     {
         public string Name { get; set; }
     }
+
+
+
+
+
+
+
+
     public class Trainer
     {
-        private Organization org = new Organization();
-
+        //private Organization org = new Organization();
+        public Organization Organization { get; set; }
         //private List<Trainee> Trainees = new List<Trainee>(); // Field
 
         public List<Trainee> Trainees { get; set; } = new List<Trainee>();
         public List<Training> Trainings { get; set; } = new List<Training>();
         public string GetOrganization()
         {
-            return org.Name;
+            return Organization.Name;
         }
     }
 
@@ -41,6 +87,33 @@
         public List<Trainee> Trainees { get; set; } = new List<Trainee>();
 
         public Course Course { get; set; }
+
+
+        public string GetTrainingOrgnizationName()
+        {
+            return Trainer.GetOrganization();
+        }
+
+        public int GetNumberOfTrainees()
+        {
+            return Trainees.Count;
+        }
+
+        public int GetTrainingDuration()
+        {
+            int duration = 0;
+            // for each module
+            foreach (Module module in Course.Modules)
+            {
+                // for each unit in a moudule
+                foreach (Unit unit in module.Units)
+                {
+                    //duration += unit.Duration;
+                    duration = duration + unit.Duration;
+                }
+            }
+            return duration;
+        }
     }
 
     public class Course
