f2681d51f976cab6af68f0d2fcf1772c3389e5a5
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-07-24 10:27:41 +0530

    mt

---
+++ MTDemo4/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace MTDemo4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(M1);
+            t1.Start();
+            Task tt1 = new Task(M1);
+            tt1.Start();
+
+            //Thread t2 = new Thread(M2);
+            Task tt2 = new Task(() => { M2(345); });
+            tt2.Start();
+
+            Task<int> tt3 = new Task<int>(M3);
+            tt3.Start();
+            //sdfsddsf
+            //sdfsdfsdf
+            //sdfsdfsdf
+            int r = tt3.Result;
+            //fsdfsdf
+            Task<int> tt4 = new Task<int>(() => { return M4(1); });
+            tt4.Start();
+
+            //sdfsdf
+            //sdfsdf
+            r = tt4.Result;
+        }
+
+
+
+        public static void M1() { }
+        public static void M2(int i) { }
+        public static int M3() { return 0; }
+        public static int M4(int i) { return i; }
+    }
+}
---
+++ MTDemo5/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MTDemo5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // i want to store 5M numbers
+            LargeData ld = new LargeData();
+            //ld.Fill(); // fills 5M numbers
+            //ld.Fill(); // fills 5M numbers
+            Parallel.Invoke(ld.Fill, ld.Fill);
+            //Thread t1 = new Thread(ld.Fill);
+            //t1.Start();
+            //Thread t2 = new Thread(ld.Fill);
+            //t2.Start();
+            //t1.Join();
+            //t2.Join();
+            Console.WriteLine("Filled :" + ld.Data.Count);
+        }
+    }
+
+    class LargeData
+    {
+        //public Stack<int> Data = new Stack<int>();
+        //System.Collections.Generic.Stack<int> Data = new Stack<int>();
+        public System.Collections.Concurrent.ConcurrentStack<int> Data = new System.Collections.Concurrent.ConcurrentStack<int>();
+        //[MethodImpl(MethodImplOptions.Synchronized)]
+        public void Fill()
+        {
+            for (int i = 1; i <= 5000000; i++)
+            {
+                //Monitor.Enter(this);
+                //lock (this)
+                //{
+                Data.Push(i); // hot-spots / critical section
+                //}
+                //Monitor.Exit(this);
+            }
+        }
+
+    }
+}
