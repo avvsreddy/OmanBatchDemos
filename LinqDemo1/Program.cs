8cc37dc604e5fe348509faa7cd2b4d128249d1f2
Venkat Shiva Reddy <venkatshiva.reddy@gmail.com>
2023-08-01 13:08:08 +0530

    linq

---
+++ LinqDemo1/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace LinqDemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Linq To Objects Demos
+
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            // extract all even numbers and store in a separate list and display
+
+            // table:numbers
+            // column: number
+            // sql select: select number from numbers where number mod 2 = 0;
+
+            // linq to objects way
+            var evenNumbers1 = from n in numbers
+                               where n % 2 == 0
+                               select n; // linq expression
+
+
+            //Func<int,bool> filter = new Func<int,bool>(IsEven);
+
+            //var evenNumbers2 = numbers.Where(filter);
+
+            var evenNumbers3 = numbers.Where(n => n % 2 == 0);
+
+            foreach (var item in evenNumbers1)
+            {
+                Console.WriteLine(item);
+            }
+            // Traditional way
+            //List<int> evens = new List<int>();
+            //foreach (var number in numbers)
+            //{
+            //    if (number % 2 == 0)
+            //        evens.Add(number);
+            //}
+            //// display
+            //foreach (var item in evens)
+            //{
+            //    Console.WriteLine(item);
+            //}
+        }
+
+        //public static bool IsEven(int n)
+        //{
+        //    return n % 2 == 0;
+        //}
+    }
+}
--- NewFeaturesDemo/Program.cs
+++ NewFeaturesDemo/Program.cs
@@ -6,30 +6,29 @@
         //var a=10;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Product p = new Product { ID = 111, Name = "IPhone", Price = 999999 };
+            p.ID = 1;
+            p.Price = 99;
+            Console.WriteLine(p.ID);
+            p.ID = 2;
+            Console.WriteLine(p.ID);
 
-            // var = Local variable type inference
-
-            // var with initial data 
-            // var with non null data
-            // var only for local variable declarations
-            // var not with parameters of a method
-            // var cant be with return types
-
-            // Anonymous Objects
-
+            Item i1 = new Item(111, "IPhone", 99999);
+            i1.Price = 0;
 
 
         }
-
     }
 
     class Product
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public int Price { get; set; }
+
+        public int ID { get; init; }
+        public string Name { get; init; }
+        public int Price { get; init; }
     }
+
+    record Item(int ID, string Name, int Price);
 }
 
 
